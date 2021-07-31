e9a46715c04988a6d2aa3b29d28d828da508b464
khaledSARRAJ <73292855+khaledSARRAJ@users.noreply.github.com>
2021-07-31 20:38:00 +0200

    Update ArtEtChatApplication.java
    re -create BDD

--- src/main/java/fr/eql/projet01/ArtEtChatApplication.java
+++ src/main/java/fr/eql/projet01/ArtEtChatApplication.java
@@ -6,16 +6,25 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//@EnableFeignClients
+//@SpringBootApplication
+//public class ArtEtChatApplication extends SpringBootServletInitializer{
+//	  @Override
+//	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//	        return application.sources(ArtEtChatApplication.class);
+//	    }
+//
+//	    public static void main(String[] args) {
+//	        SpringApplication.run(ArtEtChatApplication.class, args);
+//	    }
+
+//}
 @EnableFeignClients
 @SpringBootApplication
 public class ArtEtChatApplication extends SpringBootServletInitializer{
-	  @Override
-	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	        return application.sources(ArtEtChatApplication.class);
-	    }
-
-	    public static void main(String[] args) {
-	        SpringApplication.run(ArtEtChatApplication.class, args);
-	    }
-
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(ArtEtChatApplication.class);
+		app.setAdditionalProfiles("initData");
+		app.run(args);
+	}
 }
