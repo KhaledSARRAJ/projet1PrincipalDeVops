08930781a9ca850b175bb5baa76441c2d62db78d
khaled <khaledinat@gmail.com>
2021-08-01 19:17:26 +0200

    gerer les conflits

--- src/main/java/fr/eql/projet01/web/AdministrateurController.java
+++ src/main/java/fr/eql/projet01/web/AdministrateurController.java
@@ -38,6 +38,7 @@ import fr.eql.projet01.service.UtilisateurService;
 @CrossOrigin(origins = "*")
 //@CrossOrigin (origins = { "http://localhost:4300"}) 
 //@CrossOrigin (origins = { "http://3.144.32.173:8080"}) 
+//@CrossOrigin (origins = { "http://localhost:4300"}) 
 @RequestMapping(value = "/administrateur", headers = "Accept=application/json")
 public class AdministrateurController {
 
